7747c92b34f8b9003a8eba46e72bfc2456593a32
Alan Pecenkovic <alanquad@yahoo.com>
2021-11-27 21:36:02 +0100

    made AddUser.js with submit form inputs and added state and event handlers

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,10 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "../UI/Button";
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
 
-const AddUser = () => {
+const AddUser = props => {
+  const [userName, setUserName] = useState("");
+  const [age, setAge] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (userName.trim().length === 0 || age.trim().length === 0) {
+      return;
+    }
+    if (+age < 1) {
+      return;
+    }
+    console.log(userName, age);
+    setUserName("");
+    setAge("");
+  };
+
+  const handleChangeUsername = e => {
+    setUserName(e.target.value);
+  };
+
+  const handleChangeAge = e => {
+    setAge(e.target.value);
+  };
+
   return (
-    <div>
-      <p>A new user</p>
-    </div>
+    <Card className={classes.input}>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username</label>
+        <input
+          value={userName}
+          id="username"
+          type="text"
+          onChange={handleChangeUsername}
+        />
+        <label htmlFor="age">Age (Years)</label>
+        <input value={age} id="age" type="number" onChange={handleChangeAge} />
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
   );
 };
 
